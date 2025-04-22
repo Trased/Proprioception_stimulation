dd7a91fbc259ff8f50a4be15feedd5261962ce71
Trased <trased0998@gmail.com>
2025-04-22 20:26:45 +0300

    Upd

--- src/controller/controller.ino
+++ src/controller/controller.ino
@@ -3,11 +3,71 @@
 
 Haptic_Driver hapDrive;
 
-void setup() {
+// Test variables, will be modified at next tests
+const int position = 1;
+const int frequency = 80;
+const int weight = 17;
+const int vibrationValues[] = {0, 10, 20, 30, 40, 50, 60, 70};
+const int delayValues[] = {100, 200, 300};
+const int maxValues = 5;
+
+void waitForReadySignal()
+{
+  while (true)
+  {
+    if (Serial.available() > 0)
+    {
+        break;
+    }
+  }
+}
+
+void sendToPC(int pos, int freq, int weight, String patternString)
+{
+  String data = String(pos) + ", " + String(freq) + ", " + String(weight) + ", " + "\"" + patternString + "\"";
+  Serial.println(data);
+}
+
+String generateVibrationPattern()
+{
+  int numValues = random(1, maxValues + 1);
+  
+  int pattern[numValues];
+  int delayPattern[numValues];
+  
+  for (int i = 0; i < numValues; i++)
+  {
+    pattern[i] = vibrationValues[random(0, 8)];
+    delayPattern[i] = delayValues[random(0, 3)];
+  }
+
+  String patternString = "";
+  for (int i = 0; i < numValues; i++)
+  {
+    patternString += String(delayPattern[i]) + "ms " + String(pattern[i]) + "%";
+    if (i < numValues - 1)
+    {
+      patternString += ", ";
+    }
+  }
+
+  for (int i = 0; i < numValues; i++)
+  {
+    hapDrive.setVibrate(pattern[i]);
+    delay(delayPattern[i]);
+  }
+  hapDrive.setVibrate(0);
+
+  return patternString;
+}
+
+void setup()
+{
   Wire.begin();
   Serial.begin(115200);
 
-  if (!hapDrive.begin()) {
+  if (!hapDrive.begin())
+  {
     Serial.println("Haptic Driver initialization failed!");
     while (1);
   }
@@ -22,31 +82,48 @@ void setup() {
   motorSettings.impedance = 8.0;
   motorSettings.lraFreq = 80;
 
-  if (!hapDrive.setMotor(motorSettings)) {
+  if (!hapDrive.setMotor(motorSettings))
+  {
     Serial.println("Failed to configure actuator.");
     while (1);
   }
 
   Serial.println("Custom settings applied successfully.");
 
-  if (!hapDrive.setOperationMode(DRO_MODE)) {
+  if (!hapDrive.setOperationMode(DRO_MODE))
+  {
     Serial.println("Failed to set operation mode.");
     while (1);
   }
-  hapDrive.enableFreqTrack(true);
-  hapDrive.enableAcceleration(true);
-  hapDrive.enableRapidStop(true);
-  hapDrive.setVibrate(10);
+  hapDrive.enableFreqTrack(false);
+  hapDrive.enableAcceleration(false);
+  hapDrive.enableRapidStop(false);
+
+
   Serial.println("Driver ready.");
+
+  waitForReadySignal();
 }
 
-void loop() {
-  
-  delay(1000);
-  uint8_t irqEvent = hapDrive.getIrqEvent();
-  if (irqEvent != HAPTIC_SUCCESS) {
-    Serial.print("Fault detected: ");
-    Serial.println(irqEvent, HEX);
-    hapDrive.clearIrq(irqEvent);
+const int iterations = 10;
+void loop()
+{
+  for(int i = 0; i < iterations; i ++)
+  {
+    String patternString = generateVibrationPattern();
+    sendToPC(position, frequency, weight, patternString);
+
+    // wait for next iteration
+    delay(3000);
+
+    uint8_t irqEvent = hapDrive.getIrqEvent();
+    if (irqEvent != HAPTIC_SUCCESS)
+    {
+      hapDrive.clearIrq(irqEvent);
+      hapDrive.setOperationMode(DRO_MODE);
+    }
   }
+  Serial.println("random data to end the pc script");
+  Serial.flush();
+  waitForReadySignal();
 }
---
+++ src/data_automation/script.py
@@ -0,0 +1,58 @@
+import serial
+import pandas as pd
+import os
+
+data_list = []
+output_file = 'data_output.xlsx'
+file_exists = os.path.isfile(output_file)
+
+ser = serial.Serial('COM10', 115200, timeout=1)
+while True:
+    print("Waiting for data")
+    raw = ser.readline()
+    if not raw:
+        continue
+
+    if b"Driver ready." in raw:
+        print("Received Driver ready!")
+        break
+
+print("Sending READY signal to Arduino...")
+ser.write(b"READY\n")
+
+counter = 0
+while True:
+    if ser.in_waiting > 0:
+        data = ser.readline().decode('utf-8').strip()
+        
+        if data.lower() == "stop":
+            break
+
+        print(f"{counter}: Recevied {data}")
+        parts = data.split(',', 3)
+        if len(parts) != 4:
+            df = pd.DataFrame(data_list, columns=['Pozitie', 'Frecventa (Hz)', 'Masa (g)', 'Pattern vibratie'])
+
+            if file_exists:
+                with pd.ExcelWriter(output_file, mode='a', engine='openpyxl', if_sheet_exists='overlay') as writer:
+                    df.to_excel(writer, index=False, header=False, startrow=writer.sheets['Sheet1'].max_row)
+            else:
+                df.to_excel(output_file, index=False)
+
+            print(f"Data written to {output_file}")
+        else:
+            counter += 1
+            pos_s, freq_s, wt_s, pattern_s = parts
+            try:
+                position = int(pos_s.strip())
+                frequency = float(freq_s.strip())
+                weight    = float(wt_s.strip())
+                # strip off the surrounding quotes, if any
+                vibration_pattern = pattern_s.strip().strip('"')
+
+                data_list.append([position, frequency, weight, vibration_pattern])
+            except ValueError as e:
+                print(f"Error parsing data: {data}")
+                break
+
+
