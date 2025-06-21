e9accaa44bbaddcabfd29b1217317060e5204b85
Trased <trased0998@gmail.com>
2025-06-21 19:03:47 +0300

    Remove project from repo

--- cpp_project/bgm_proj/GNU ARM v12.2.1 - Debug/app.d
+++
@@ -1,3 +0,0 @@
-app.o: ../app.cpp \
- c:\users\v\simplicitystudio\sdks\simplicity_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h
-c:\users\v\simplicitystudio\sdks\simplicity_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h:
--- cpp_project/bgm_proj/GNU ARM v12.2.1 - Debug/simplicity_sdk_2024.12.2/platform/service/system/src/sl_system_init.d
+++
@@ -1,6 +0,0 @@
-simplicity_sdk_2024.12.2/platform/service/system/src/sl_system_init.o: \
- C:/Users/V/SimplicityStudio/SDKs/simplicity_sdk/platform/service/system/src/sl_system_init.c \
- c:\users\v\simplicitystudio\sdks\simplicity_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h \
- D:\01-University\00-Github\Proprioception_stimulation\cpp_project\bgm_proj\autogen/sl_event_handler.h
-c:\users\v\simplicitystudio\sdks\simplicity_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h:
-D:\01-University\00-Github\Proprioception_stimulation\cpp_project\bgm_proj\autogen/sl_event_handler.h:
